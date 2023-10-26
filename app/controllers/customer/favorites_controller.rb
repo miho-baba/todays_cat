50b21a2189a6524641fc111b13b9f3b401d8db76
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-10-26 13:00:11 +0000

    コメントアウトの記述削除

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -4,26 +4,12 @@ class Customer::FavoritesController < ApplicationController
     @photo = Photo.find(params[:photo_id])
     favorite = @photo.favorites.new(customer_id: current_customer.id)
     favorite.save
-
-    #非同期通信化のためコメントアウトにする
-      #if params[:index] == "true"
-      #redirect_to customer_photos_path
-      #elsif params[:index] == "false"
-       #redirect_to customer_photo_path(@photo)
-      #end
   end
 
   def destroy
     @photo = Photo.find(params[:photo_id])
     favorite = current_customer.favorites.find_by(photo_id: @photo.id)
     favorite.destroy
-
-    #非同期通信化のためコメントアウトにする
-      #if params[:index] == "true"
-      #redirect_to customer_photos_path
-      #elsif params[:index] == "false"
-       #redirect_to customer_photo_path(@photo)
-      #end
   end
 
 end
--- app/controllers/customer/photos_controller.rb
+++ app/controllers/customer/photos_controller.rb
@@ -9,12 +9,6 @@ class Customer::PhotosController < ApplicationController
 
   def index
   @photos = Photo.all.page(params[:page]).per(6).order(created_at: :desc) # 1ページに6人の会員を表示し、最新のものから順に並べ替える
-  #@photos = Photo.includes(:favorites)
-                  #.left_joins(:favorites)
-                  #.group('photos.id')
-                  #.order(created_at: :desc)# 写真を最新のものから順に並び替える
-                  #.page(params[:page]) # 1ページに6個の写真を表示
-                  #.per(6)
   @photo = Photo.new
   end
 
@@ -26,11 +20,11 @@ class Customer::PhotosController < ApplicationController
     @photo = Photo.new(photo_params)
     @photo.customer_id = current_customer.id
     if @photo.save
-    flash[:notice] = "写真の投稿に成功しました！"#動作ok!写真投稿できたら表示する記述
+    flash[:notice] = "写真の投稿に成功しました！"#写真投稿できたら表示する記述
       redirect_to customer_photo_path(@photo.id)
     else
       @customer = current_customer
-      flash[:alert] = "写真の投稿に失敗しました。"  # 動作ok!写真投稿失敗した記述
+      flash[:alert] = "写真の投稿に失敗しました。"  #写真投稿失敗した記述
       @photos = @customer.photos.page(params[:page]) # 1ページに12個の写真を表示
                           .per(12)
       render 'customer/customers/mypage'
@@ -43,13 +37,10 @@ class Customer::PhotosController < ApplicationController
 
   def update
     @photo = Photo.find(params[:id])
-    #if @photo.customer_id == current_customer.id
       if @photo.update(photo_params)
       flash[:notice] = "編集の更新に成功しました！"#写真の編集に成功したら表示する記述
       redirect_to customer_photo_path(@photo.id)
       else
-      #render :editredirect_to customer_photo_path(@photo.id)
-      #render :edit
       flash[:alert] = "編集の更新に失敗しました。"
       render 'customer/photos/edit' # 編集ページにリダイレクトし、エラーメッセージを表
       end
