8599c4b02147a24906843cad2bdcd71c5f5ad348
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-11-14 05:49:01 +0000

    RSpecテストの実装中

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
 
   namespace :user do
     root "homes#top"
+      root "user/homes#top"
     resources :customers, only: [:show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- spec/system/01_before_login_spec.rb
+++ spec/system/01_before_login_spec.rb
@@ -1,14 +1,34 @@
-require 'rails_helper'
+# ./spec/system/01_before_login_spec.rb
 
 describe '[STEP1] ユーザログイン前のテスト' do
   describe 'トップ画面のテスト' do
     before do
-      visit user_root_path
+      visit root_path  # user_root_pathではなくroot_pathを使用
     end
 
     context '表示内容の確認' do
       it 'URLが正しい' do
-        expect(user_root_path).to eq '/user'
+        expect(current_path).to eq '/'
+      end
+
+      it 'Log inリンクが表示される: 薄緑のボタンの表示が「ログイン」である' do
+        log_in_link = find_all('a')[5].text
+        expect(log_in_link).to match(/Log in/)
+      end
+
+      it 'Log inリンクの内容が正しい' do
+        log_in_link = find_all('a')[5]
+        expect(log_in_link['href']).to eq new_customer_session_path  # href属性を直接比較
+      end
+
+      it 'Sign upリンクが表示される: 薄緑のボタンの表示が「新規登録」である' do
+        sign_up_link = find_all('a')[6].text
+        expect(sign_up_link).to match(/Sign up/)
+      end
+
+      it 'Sign upリンクの内容が正しい' do
+        sign_up_link = find_all('a')[6]
+        expect(sign_up_link['href']).to eq new_customer_registration_path  # href属性を直接比較
       end
     end
   end
