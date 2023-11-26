3624a3b8b6b047530c26f552860382149619d189
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-11-26 23:31:12 +0000

    devise.ja.ymlに記述を追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin! # 管理者の認証が必要なアクションに対して適用する
+  before_action :set_customer, only: [:show, :update]
   def index
     # ページネーションの記述
     @customers = Customer.page(params[:page]).per(6) # 1ページに6人の会員を表示
@@ -21,6 +22,10 @@ class Admin::CustomersController < ApplicationController
   end
 
   private
+    def set_customer
+    @customer = Customer.find(params[:id])
+    end
+
     def customer_params
       params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :email, :is_deleted)
     end
